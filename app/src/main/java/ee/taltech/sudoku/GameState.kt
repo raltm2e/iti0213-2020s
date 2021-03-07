a05b70bb7527d0935f124b762f58f05400829a0e
Robert <roaltm@ttu.ee>
2021-03-07 20:26:10 +0200

    Change fields to suit sudoku

--- app/src/main/java/ee/taltech/sudoku/GameState.kt
+++ app/src/main/java/ee/taltech/sudoku/GameState.kt
@@ -5,13 +5,15 @@ class GameState {
     var gameBoard: String = ""
     var difficulty: String = ""
     var timeSpent: Int = 0
+    var gameFinished: Boolean = false
 
-    constructor(gameBoard: String, scoreBoard: String, timeSpent: Int): this(0, gameBoard, scoreBoard, timeSpent)
+    constructor(gameBoard: String, scoreBoard: String, timeSpent: Int, gameFinished: Boolean): this(0, gameBoard, scoreBoard, timeSpent, gameFinished)
 
-    constructor(id: Int, gameBoard: String, difficulty: String, timeSpent: Int) {
+    constructor(id: Int, gameBoard: String, difficulty: String, timeSpent: Int, gameFinished: Boolean) {
         this.id = id
         this.gameBoard = gameBoard
         this.difficulty = difficulty
         this.timeSpent = timeSpent
+        this.gameFinished = gameFinished
     }
 }
