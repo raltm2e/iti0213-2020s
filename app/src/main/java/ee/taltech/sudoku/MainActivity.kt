c5f556080bfb725e20200ad5a61013cf5ebfa0e4
Robert <roaltm@ttu.ee>
2021-03-08 22:04:00 +0200

    Add iterating onClick

--- app/src/main/java/ee/taltech/sudoku/MainActivity.kt
+++ app/src/main/java/ee/taltech/sudoku/MainActivity.kt
@@ -2,6 +2,12 @@ package ee.taltech.sudoku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import ee.taltech.sudoku.sudokulib.GRID_SIZE
+import ee.taltech.sudoku.sudokulib.Level
+import ee.taltech.sudoku.sudokulib.Sudoku
 
 class MainActivity : AppCompatActivity() {
     private lateinit var gameStateRepository: GameStateRepository
@@ -10,10 +16,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         gameStateRepository = GameStateRepository(this).open()
+
+
+        var generatedSudokuBoard = Sudoku.Builder().setLevel(Level.JUNIOR).build()
+        Log.d("BOARD", generatedSudokuBoard.grid[0].contentToString())
+        generateGameBoard(generatedSudokuBoard.grid)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         gameStateRepository.close()
+    }
+
+    private fun generateGameBoard(generatedGameArray: Array<IntArray>) {
+
+
+        for (i in 0 until GRID_SIZE) {
+            for (j in 0 until GRID_SIZE) {
+                Log.d("ELEMENT", generatedGameArray[i][j].toString())
+            }
+        }
+    }
+
+    fun buttonPressed(view: View) {
+        val button1 = view as Button
+        button1.text = iterateNumber(button1)
+    }
+
+    fun iterateNumber(button: Button): String {
+        if (button.text == "") {
+            return "1"
+        }
+        val buttonValue = button.text.toString().toInt() + 1
+        if (buttonValue == 10) {
+            return ""
+        }
+        return buttonValue.toString()
     }
 }
