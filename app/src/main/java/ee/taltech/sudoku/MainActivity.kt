1d8c623a467295bee0cfa65d26689f27d2f232b5
Robert <roaltm@ttu.ee>
2021-03-09 23:34:36 +0200

    Some work for generating sudoku board, not done yet

--- app/src/main/java/ee/taltech/sudoku/MainActivity.kt
+++ app/src/main/java/ee/taltech/sudoku/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 import ee.taltech.sudoku.sudokulib.GRID_SIZE
 import ee.taltech.sudoku.sudokulib.Level
 import ee.taltech.sudoku.sudokulib.Sudoku
@@ -16,11 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         gameStateRepository = GameStateRepository(this).open()
-
-
-        var generatedSudokuBoard = Sudoku.Builder().setLevel(Level.JUNIOR).build()
-        Log.d("BOARD", generatedSudokuBoard.grid[0].contentToString())
-        generateGameBoard(generatedSudokuBoard.grid)
     }
 
     override fun onDestroy() {
@@ -28,14 +24,49 @@ class MainActivity : AppCompatActivity() {
         gameStateRepository.close()
     }
 
-    private fun generateGameBoard(generatedGameArray: Array<IntArray>) {
+    fun generateGameBoard(view: View) {
+        val layouts: ConstraintLayout = findViewById(R.id.gameboard)
+        val gameBoardMapSmall: MutableMap<Int, MutableList<Button>> = mutableMapOf<Int, MutableList<Button>>()
+        for (i in 0 until layouts.childCount) {
+            val buttons9: MutableList<Button> = ArrayList()
+            // Get elements of single 3x3 square, add them to a list of buttons
+            // This is necessary to add values to them obtained by board generator
+            val singleSquare = layouts.getChildAt(i) as ConstraintLayout
+            for (j in 0 until singleSquare.childCount) {
+                buttons9.add(singleSquare.getChildAt(j) as Button)
+            }
+            // Add buttons into dictionary so they are in order
+            gameBoardMapSmall[i + 1] = buttons9
+        }
 
+        // Generate a sudoku board
+        val generatedSudokuBoard = Sudoku.Builder().setLevel(Level.JUNIOR).build()
+        val generatedGameArray = generatedSudokuBoard.grid
 
-        for (i in 0 until GRID_SIZE) {
-            for (j in 0 until GRID_SIZE) {
-                Log.d("ELEMENT", generatedGameArray[i][j].toString())
+        // Add generated values to each buttons text
+        // GRID_SIZE + 1 is because button id starts from 11 and ends at 99
+        for (i in 1 until GRID_SIZE + 1) {
+            for (j in 1 until GRID_SIZE + 1) {
+                val buttonList9: MutableList<Button> = gameBoardMapSmall[i]!!
+                val buttonToChange: Button = buttonList9[j]
+                buttonToChange.text = generatedGameArray[i][j].toString()
+//                Log.d("ButtonID", buttonId)
+//                buttonId.text = generatedGameArray[i][j].toString()
             }
         }
+    }
+
+    private fun getButtonById(id: String, buttons: MutableList<Button>): Button {
+        for (i in 0 until buttons.size) {
+            Log.d("ButtonID", buttons[i].id.toString())
+            if (buttons[i].id.toString() == id) {
+                return buttons[i]
+            }
+        }
+        // I don't know how to return empty button
+        // This is just a placeholder, it should never go here
+        Log.d("ERROR", "getButtonById cant get correct amount of buttons to iterate")
+        return buttons[-1]
     }
 
     fun buttonPressed(view: View) {
