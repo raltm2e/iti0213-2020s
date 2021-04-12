7a0369a52e6985c34b263c66c3b99057d9fa941a
robert <robert@roberts-air.lan>
2021-04-12 16:14:59 +0300

    Add iOS

---
+++ ios/sudoku3/sudoku3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sudoku3
+//
+//  Created by robert on 12/04/2021.
+//  Copyright © 2021 robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
