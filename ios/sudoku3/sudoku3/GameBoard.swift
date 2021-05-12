68349b54394ca1b1da3842e4d5967eb8d2b27a20
robert <robert@74d8-e7f6-4cdc-140c-eb80-8078-07d0-2001.dyn.estpak.ee>
2021-05-12 12:47:47 +0300

    Add another easy board

--- ios/sudoku3/sudoku3/GameBoard.swift
+++ ios/sudoku3/sudoku3/GameBoard.swift
@@ -12,22 +12,23 @@ class GameBoard {
     
     let gameboardeasy1 = ["---68-19-", "26--7---4", "7-1-9-5--", "82---4-5-", "1--6-2--3", "-4-9---28", "--9-4-7-3", "3---5--18", "-74-36---"]
     let gameboardeasy1solution = ["435682197", "269571834", "781493562", "826374951", "195682743", "347915628", "519248763", "326957418", "874136259"]
-    //let gameboardseasy = [gameboardeasy1]
+    
+    let gameboardeasy2 = ["1--73----", "489-----1", "--6-4-295", "--75----6", "12-7-3-95", "6----87--", "914-2-8--", "6-----512", "----37--4"]
+    let gameboardeasy2solution = ["152739468", "489256371", "376841295", "387591246", "124763895", "659428713", "914625873", "637948512", "582137964"]
     
     func getsolutions() -> Array<Array<String>> {
-        return [gameboardeasy1solution]
+        return [gameboardeasy1solution, gameboardeasy2solution]
     }
     
     func getrandomboard(difficulty: String) -> Array<String> {
-        """
-if (difficulty == "easy") {
+        let gameboardseasy = [gameboardeasy1, gameboardeasy2]
+        if (difficulty == "easy") {
             if (gameboardseasy.count > 1) {
-                let randomnumber = Int.random(in: 0..<gameboardseasy.count - 1)
+                let randomnumber = Int.random(in: 0..<gameboardseasy.count)
                 return gameboardseasy[randomnumber]
             }
-            return gameboardeasy1
         }
-"""
+        print("Something bad")
         //TODO create hard board
         return gameboardeasy1
     }
